4d4f8c2aa3b4f7c00a075ebc01c179e392a5e57d
Ilya N. Zykin <ilya.zykin@toptal.com>
2016-06-08 13:54:18 +0300

    ~~~

--- LOADERS/second-loader/index.js
+++ LOADERS/second-loader/index.js
@@ -2,6 +2,7 @@ module.exports = function(content) {
   log("\n********************************")
   log('SECOND', content.slice(0, 200))
   log('\n********************************')
+
   this.cacheable();
   return content
 }
