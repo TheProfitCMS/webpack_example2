c0f7775b52039a3dca82916c8fbef9a6bb4d9459
Ilya N. Zykin <ilya.zykin@toptal.com>
2016-06-08 11:16:08 +0300

    ~~~

--- README.md
+++ README.md
@@ -14,3 +14,13 @@ npm install node-inspector --save
 ```
 
 npm run build
+
+### Debugger
+
+* backtrace, bt - Print backtrace of current execution frame
+* list(5) - List scripts source code with 5 line context (5 lines before and after)
+* watch(expr) - Add expression to watch list
+* unwatch(expr) - Remove expression from watch list
+* watchers - List all watchers and their values (automatically listed on each breakpoint)
+* repl - Open debugger's repl for evaluation in debugging script's context
+* exec expr
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,6 @@
 'use strict';
 
 const webpack = require('webpack')
-
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-
 let webPackConfig = {}
 
 webPackConfig.plugins = []
@@ -29,38 +26,10 @@ webPackConfig.resolve.alias = {
   js_path:  __dirname + "/__SRC/assets/js/"
 }
 
-/*
-  EXTRACT SPECIFIC CSS FILES FROM ENTRY POINTS
-*/
-
-let extractSpecificCSSFiles = {
-  css: {
-    'main.css': 'assets/css/MAIN.css'
-  },
-  sass: {
-    'first-entry.sass': 'assets/css/FIRST.css'
-  },
-  scss: {
-    'second-entry.scss': 'assets/css/SECOND.css'
-  }
-}
-
-function extractCSSFilesExclude(extractSpecificCSSFiles, css_type){
-  if(!css_type) return null
-  var patterns = []
-
-  for(var filename_pattern in extractSpecificCSSFiles[css_type]){
-    var regexp = new RegExp(filename_pattern)
-    if(patterns.indexOf(filename_pattern) == -1) patterns.push(regexp)
-  }
-
-  if(patterns.length == 0) return null;
-  return patterns
-}
-
-/*
-  ~ EXTRACT SPECIFIC CSS FILES FROM ENTRY POINTS
-*/
+webPackConfig.resolveLoader = webPackConfig.resolveLoader || {}
+webPackConfig.resolveLoader.modulesDirectories = [
+  '.', 'node_modules', 'web_loaders', 'web_modules', 'node_loaders'
+]
 
 webPackConfig.module = webPackConfig.module || {}
 webPackConfig.module.loaders = [
@@ -73,71 +42,19 @@ webPackConfig.module.loaders = [
 
   {
     test:   /\.css$/,
-    loaders: ['style', 'css?sourceMap'],
-    exclude: extractCSSFilesExclude(extractSpecificCSSFiles, 'css')
+    loaders: ['first-loader', 'style', 'css?sourceMap']
   },
 
   {
     test: /\.sass$/,
-    loaders: ['style', 'css?sourceMap', 'sass?sourceMap&indentedSyntax'],
-    exclude: extractCSSFilesExclude(extractSpecificCSSFiles, 'sass')
+    loaders: ['style', 'css?sourceMap', 'sass?sourceMap&indentedSyntax']
   },
 
   {
     test: /\.scss$/,
-    loaders: ['style', 'css?sourceMap', 'sass?sourceMap'],
-    exclude: extractCSSFilesExclude(extractSpecificCSSFiles, 'scss')
+    loaders: ['style', 'css?sourceMap', 'sass?sourceMap']
   }
 ]
-
-/*
-  EXTRACT SPECIFIC CSS FILES FROM ENTRY POINTS
-*/
-for(var css_type in extractSpecificCSSFiles){
-  for(var filename_pattern in extractSpecificCSSFiles[css_type]) {
-    var dist_filename = extractSpecificCSSFiles[css_type][filename_pattern];
-
-    if(css_type == 'css'){
-      console.log("CSS", filename_pattern, dist_filename)
-
-      let css_extract = new ExtractTextPlugin(filename_pattern, dist_filename)
-      webPackConfig.plugins.push(css_extract)
-
-      webPackConfig.module.loaders.push({
-        test:    extractCSSFilesExclude(extractSpecificCSSFiles, 'css'),
-        loader:  css_extract.extract(['css?sourceMap'])
-      })
-    }
-
-    if(css_type == 'sass') {
-      console.log("SASS", filename_pattern, dist_filename)
-
-      let sass_extract = new ExtractTextPlugin(filename_pattern, dist_filename)
-      webPackConfig.plugins.push(sass_extract)
-
-      webPackConfig.module.loaders.push({
-        test:    extractCSSFilesExclude(extractSpecificCSSFiles, 'sass'),
-        loader: sass_extract.extract(['css?sourceMap', 'sass?sourceMap&indentedSyntax'])
-      })
-    }
-
-    if(css_type == 'scss') {
-      console.log("SCSS", filename_pattern, dist_filename)
-
-      let scss_extract = new ExtractTextPlugin(filename_pattern, dist_filename)
-      webPackConfig.plugins.push(scss_extract)
-
-      webPackConfig.module.loaders.push({
-        test:    extractCSSFilesExclude(extractSpecificCSSFiles, 'scss'),
-        loader: scss_extract.extract(['css?sourceMap', 'sass?sourceMap'])
-      })
-    }
-  }
-}
-
-/*
-  ~ EXTRACT SPECIFIC CSS FILES FROM ENTRY POINTS
-*/
 
 webPackConfig.watchOptions = {
   aggregateTimeout: 100
