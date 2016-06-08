9010d82163ead50e3b9fb07ce109249678a282d0
Ilya N. Zykin <ilya.zykin@toptal.com>
2016-06-08 13:52:52 +0300

    ~~~

--- LOADERS/first-loader/index.js
+++ LOADERS/first-loader/index.js
@@ -1,6 +1,6 @@
 module.exports = function(content) {
   log("\n********************************")
-  log('SECOND', content.slice(0, 20))
+  log('FIRST', content.slice(0, 20))
   log('\n********************************')
   this.cacheable();
   return content
--- LOADERS/second-loader/index.js
+++ LOADERS/second-loader/index.js
@@ -5,3 +5,43 @@ module.exports = function(content) {
   this.cacheable();
   return content
 }
+
+// for(var i in this){ log(i) }
+// =>
+// this.data
+// this.options
+// this.query
+// this.resourcePath
+
+// this.inputValue
+// this.async
+// this.callback
+// this.version
+// this.context
+// this.loaders
+// this.loaderIndex
+// this.resource
+// this.resourcePath
+// this.resourceQuery
+// this.emitWarning
+// this.emitError
+// this.exec
+// this.resolve
+// this.resolveSync
+// this.cacheable
+// this.dependency
+// this.addDependency
+// this.addContextDependency
+// this.clearDependencies
+// this.value
+//
+// this.debug
+// this.webpack
+// this.sourceMap
+// this.emitFile
+// this._module
+// this._compilation
+// this._compiler
+// this.fs
+// this.target
+// this.loadModule
