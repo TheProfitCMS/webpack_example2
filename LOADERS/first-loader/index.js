2c86f4ebfd598d9d725bd981c50669447cb611bc
Ilya N. Zykin <ilya.zykin@toptal.com>
2016-06-21 11:52:11 +0300

    ~~~

--- LOADERS/first-loader/index.js
+++ LOADERS/first-loader/index.js
@@ -2,7 +2,7 @@ module.exports = function(content) {
   log("\n********************************")
   log('FIRST', content.slice(0, 20))
   log('\n********************************')
-
+  debugger
   this.cacheable();
   return content
 }
--- __SRC/assets/js/app.js
+++ __SRC/assets/js/app.js
@@ -2,8 +2,6 @@ import 'js_path/log.js'
 import 'js_path/hello_world.js'
 
 import 'css_path/reset.css'
-
-// SHOULD BE EXTRACTED TO SEPARATED FILES
 import 'css_path/main.css'
 import 'css_path/first-entry.sass'
 import 'css_path/second-entry.scss'
